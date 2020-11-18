44fe87c6eb9f5720a8cb0814b47c037effac4935
Cijomanuel <44916914+Cijomanuel@users.noreply.github.com>
2020-11-19 01:20:04 +0530

    Commit for disabling bootstrap

--- src/Acc.js
+++ src/Acc.js
@@ -98,7 +98,7 @@ render(){
     return(
       <div className='acc'>
         <div className="mbox">                       
-    <div  className=" col1" style={{"zIndex":"1!important"}}>
+    <div  className=" col1" >
       <h1>Account</h1> 
     </div>
     <div className="maingrid" id="M1">
